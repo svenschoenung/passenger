8f1dee985d13273ef05be15d713e92da84c881a0
Sven Schoenung <sven.schoenung@gmail.com>
2020-03-02 19:21:35 +0100

    Move password tree to own component

---
+++ src/components/PasswordsTree.vue
@@ -0,0 +1,64 @@
+<template>
+<div class="cover bg-1">
+    <q-tree
+        v-if="passwordsTree"
+        :nodes="passwordsTree"
+        :icon="icons.arrow"
+        default-expand-all
+        node-key="absPath"
+        label-key="name"
+        :selected.sync="selected"
+        selected-color="primary"
+    />
+    <q-circular-progress
+        v-else
+        indeterminate
+        size="50px"
+        :thickness="0.22"
+        color="primary"
+        track-color="grey-3"
+        class="centered-progress"
+    />
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { PasswordsModule, UIModule } from "../store";
+import icons from "@/ui/icons";
+
+@Component({
+  name: "passwords-tree",
+  components: {}
+})
+export default class PasswordsTree extends Vue {
+
+  set selected(absPath: string) {
+    UIModule.selectPasswordNode$(absPath);
+  }
+
+  get selected() {
+    return UIModule.selectedPasswordNode as string;
+  }
+
+  created() {
+    (this as any).icons = icons;
+  }
+
+  get passwordsTree() {
+    if (!(PasswordsModule.tree && PasswordsModule.tree.children)) {
+        PasswordsModule.loadTree$()
+        return null
+    }
+    return PasswordsModule.tree.children
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.centered-progress {
+  position: absolute;
+  top: calc(50% - 25px);
+  left: calc(50% - 25px);
+}
+</style>
--- src/pages/PasswordsPage.vue
+++ src/pages/PasswordsPage.vue
@@ -2,25 +2,7 @@
   <q-splitter v-model="treePaneWidthInPercent" :style="{ height: `calc(100vh - ${footerHeight}px`}">
     <template v-slot:before>
       <div class="cover bg-1">
-        <q-tree
-          v-if="passwordsTree"
-          :nodes="passwordsTree"
-          :icon="icons.arrow"
-          default-expand-all
-          node-key="absPath"
-          label-key="name"
-          :selected.sync="selected"
-          selected-color="primary"
-        />
-        <q-circular-progress
-          v-else
-          indeterminate
-          size="50px"
-          :thickness="0.22"
-          color="primary"
-          track-color="grey-3"
-          class="centered-progress"
-        />
+        <passwords-tree/>
       </div>
     </template>
     <template v-slot:after>Passwords</template>
@@ -30,43 +12,19 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import { FOOTER_HEIGHT } from "@/constants";
-import { PasswordsModule, UIModule } from "../store";
-import icons from "@/ui/icons";
+import PasswordsTree from '@/components/PasswordsTree.vue'
 
 @Component({
   name: "passwords-page",
-  components: {}
+  components: {
+    PasswordsTree
+  }
 })
 export default class PasswordsPage extends Vue {
   footerHeight = FOOTER_HEIGHT;
   treePaneWidthInPercent = 30;
-
-  set selected(absPath: string) {
-    UIModule.selectPasswordNode$(absPath);
-  }
-
-  get selected() {
-    return UIModule.selectedPasswordNode as string;
-  }
-
-  created() {
-    (this as any).icons = icons;
-  }
-
-  get passwordsTree() {
-    if (!(PasswordsModule.tree && PasswordsModule.tree.children)) {
-        PasswordsModule.loadTree$()
-        return null
-    }
-    return PasswordsModule.tree.children
-  }
 }
 </script>
 
 <style lang="scss" scoped>
-.centered-progress {
-  position: absolute;
-  top: calc(50% - 25px);
-  left: calc(50% - 25px);
-}
 </style>
